c4ea8bb9d6d42b0e50f422db44adb2d12bdd0b3a
Sean C. Sullivan <github@seansullivan.com>
2020-07-23 16:53:44 -0700

    javapoet 1.13.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.squareup:javapoet:1.12.1")
+    implementation("com.squareup:javapoet:1.13.0")
     implementation("com.vdurmont:emoji-java:5.1.0")
     implementation("org.apache.commons:commons-lang3:3.9")
 }
